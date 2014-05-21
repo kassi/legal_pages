da9d9778690a4b5a2214d812ce13e3437c2c3444
Karsten Silkenbäumer <ksi@kluks.de>
2014-05-21 21:11:04 +0200

    Remove stylesheet

--- lib/legal_pages/engine.rb
+++ lib/legal_pages/engine.rb
@@ -1,11 +1,5 @@
-
 module LegalPages
   class Engine < ::Rails::Engine
     isolate_namespace LegalPages
-
-    # initializer "legal_pages.assets.precompile" do |app|
-    #   app.config.assets.precompile += %w(legal.css)
-    # end
   end
 end
-
