d78241c410a939510e7ea755bcb8ee7754182eb3
Karsten Silkenbäumer <ksi@kluks.de>
2015-08-03 23:18:52 +0200

    Remove root_path to allow only subpages being displayed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 LegalPages::Engine.routes.draw do
-  root to: redirect('imprint')
   get 'imprint'    => 'legal#index'
   get 'disclaimer' => 'legal#disclaimer'
   get 'privacy'    => 'legal#privacy'
